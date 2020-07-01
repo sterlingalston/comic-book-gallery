edee812be4efcc232681454b4fd8a899e50f12ed
Matthew Alston <sterlingalston@gmail.com>
2020-07-01 17:33:56 -0400

    added comic book controller

---
+++ src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComicBookGallery.Controllers
+{
+    public class ComicBooksController: Controller
+    {
+
+
+    }
+}
