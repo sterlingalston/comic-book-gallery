e76216c395124dc9a4341fd26d40ba479094609a
Matthew Alston <sterlingalston@gmail.com>
2020-07-05 17:45:39 -0400

    added comic detail view

--- src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -9,22 +9,31 @@ namespace ComicBookGallery.Controllers
     public class ComicBooksController: Controller
     {
         //public ContentResult Detail() 
-        public ActionResult Detail()
+        #region
+        //public ActionResult Detail()
+        //{
+        //    if (DateTime.Today.DayOfWeek == DayOfWeek.Sunday) 
+        //    {
+        //        //return new RedirectResult("/");
+        //        return Redirect("/");
+        //    }
+
+        //    return Content("Hello from the comic books controller.");
+
+        //  /*  return new ContentResult() 
+        //    {
+        //        Content = "Hello from the comic books controller."
+        //    };*/
+        //    //return "Hello from the comic books controller.";
+        //}
+        
+        #endregion
+        
+        public ActionResult Detail() 
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday) 
-            {
-                //return new RedirectResult("/");
-                return Redirect("/");
-            }
-
-            return Content("Hello from the comic books controller.");
-
-          /*  return new ContentResult() 
-            {
-                Content = "Hello from the comic books controller."
-            };*/
-            //return "Hello from the comic books controller.";
+            return View();
         }
-
     }
+
+    
 }
