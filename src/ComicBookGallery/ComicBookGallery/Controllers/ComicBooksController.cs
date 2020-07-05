714094772ac386a88ccd160a8f351833771686ed
Matthew Alston <sterlingalston@gmail.com>
2020-07-05 12:02:37 -0400

    changing return types to methods that return the same return type, commented out previous code of the same return type that initiatites new instances of such classes

--- src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -13,13 +13,16 @@ namespace ComicBookGallery.Controllers
         {
             if (DateTime.Today.DayOfWeek == DayOfWeek.Monday) 
             {
-                return new RedirectResult("/");
+                //return new RedirectResult("/");
+                return Redirect("/");
             }
 
-            return new ContentResult() 
+            return Content("Hello from the comic books controller.");
+
+          /*  return new ContentResult() 
             {
                 Content = "Hello from the comic books controller."
-            };
+            };*/
             //return "Hello from the comic books controller.";
         }
 
